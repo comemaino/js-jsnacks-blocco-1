c8036b763b82278437fc5dba0687e8e39d3b4294
comemaino <mat.maino@gmail.com>
2022-04-19 15:09:57 +0200

    Add: jsnack4 script

---
+++ jsnack4/js/script.js
@@ -0,0 +1,22 @@
+// Chiedi un numero di 4 cifre all'utente
+// e calcola la somma di tutte le cifre che compongono il numero.
+
+
+let userNumber = parseInt(prompt("inserisci un numero di 4 cifre"));
+
+let digits = [];
+while (userNumber > 0) {
+  digits.push(userNumber % 10);
+
+  userNumber = parseInt(userNumber / 10);
+}
+console.log(userNumber);
+console.log(digits);
+
+let sum = 0;
+
+for (let i = 0; i < digits.length; i++) {
+  sum += digits[i];
+}
+
+console.log(sum);
