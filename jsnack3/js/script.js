4b39986c7905359b24c44beab9f8aeced5852a2d
comemaino <mat.maino@gmail.com>
2022-04-19 14:23:22 +0200

    Add: jsnack3 script

---
+++ jsnack3/js/script.js
@@ -0,0 +1,12 @@
+// Calcola la somma e la media dei primi 10 numeri
+
+let sum = 0;
+
+for (let i = 0; i < 10; i++) {
+  sum += i;
+}
+
+console.log(sum);
+
+const average = sum / 10;
+console.log(average);
